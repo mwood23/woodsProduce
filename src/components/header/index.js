08c37f640fb097c9fd6295f460beffc97c062f08
Marcus Wood <marcuswood@Marcuss-MBP.fios-router.home>
2017-07-05 19:15:27 -0400

    Moved scroll events into corrent component. Working on mobile nav. Just about there

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,20 +1,96 @@
+import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 import { Button } from 'react-bootstrap';
 
-const Header = () => (
-	<header className={style.header}>
-		<img src="../../assets/wplogo1.png"></img>
-		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Products</Link>
-			<Link activeClassName={style.active} href="/profile">Food Safety</Link>
-			<Link activeClassName={style.active} href="/profile">Our Story</Link>
-			<Link activeClassName={style.active} href="/profile">Case Studies</Link>
-			<Link activeClassName={style.active} href="/profile">Careers</Link>
-		</nav>
-		<Button>Get Started</Button>
-	</header>
-)
+export default class Header extends Component {
 
-export default Header
+	// This handle changing the nav bar color on scroll
+	componentDidMount() {
+		console.log(this);
+		this._bindScroll()
+	}
+
+	componentWillUnmount() {
+		this._unbindScroll()
+	}
+
+	_bindScroll = () => {
+		// Use passive event listener if available
+		let supportsPassive = false
+		try {
+			const opts = Object.defineProperty({}, 'passive', {
+				get: () => {
+					supportsPassive = true
+				}
+			})
+			window.addEventListener('test', null, opts)
+		} catch (e) {} // eslint-disable-line no-empty
+
+		window.addEventListener('scroll', this._handleScroll, supportsPassive
+			? {
+				passive: true
+			}
+			: false)
+	}
+
+	_unbindScroll = () => {
+		window.removeEventListener('scroll', this._handleScroll)
+	}
+
+	_handleScroll = () => {
+		// Ugly cross-browser compatibility
+		const top = document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop
+		// Test < 1 since Safari's rebound effect scrolls past the top
+
+		if (top < 10) {
+			const className = `${style.header}`
+			this.base.className = className
+		} else {
+			const className = `${style.header} ${style.scrolled}`
+			this.base.className = className
+		}
+	}
+
+	// This handles opening and closing the hamburger menu on mobile
+	state = { open:false };
+
+	toggle = () => this.setState({ open: !this.state.open });
+
+	// close menu on navigate
+	componentWillReceiveProps({ url }) {
+		if (url!==this.props.url && this.state.open) {
+			this.setState({ open:false });
+		}
+	}
+
+	render({ url }, { open }) {
+		return (
+			<header class={style.header}>
+				<img src="../../assets/wplogo1.png"></img>
+				<section>
+				<nav>
+					{/* Remove active class name if we don't intend on giving it a selected style */}
+						<Link activeClassName={style.active} href="/">Home</Link>
+						<Link activeClassName={style.active} href="/profile">Products</Link>
+						<Link activeClassName={style.active} href="/profile">Food Safety</Link>
+						<Link activeClassName={style.active} href="/profile">Our Story</Link>
+						<Link activeClassName={style.active} href="/profile">Case Studies</Link>
+						<Link activeClassName={style.active} href="/profile">Careers</Link>
+				</nav>
+			</section>
+				<Button>Get Started</Button>
+				<Hamburgler open={open} onClick={this.toggle} />
+			</header>
+		);
+	}
+}
+
+// hamburgler menu
+const Hamburgler = ({ open, ...props }) => (
+	<div class={style.hamburgler} open={open} {...props}>
+		<div class={style.hb1} />
+		<div class={style.hb2} />
+		<div class={style.hb3} />
+	</div>
+);
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,59 +4,12 @@ import Header from '../../components/header'
 
 export default class Home extends Component {
 
-	componentDidMount() {
-		console.log(this);
-		this._bindScroll()
-	}
-
-	componentWillUnmount() {
-		this._unbindScroll()
-	}
-
-	_bindScroll = () => {
-		// Use passive event listener if available
-		let supportsPassive = false
-		try {
-			const opts = Object.defineProperty({}, 'passive', {
-				get: () => {
-					supportsPassive = true
-				}
-			})
-			window.addEventListener('test', null, opts)
-		} catch (e) {} // eslint-disable-line no-empty
-
-		window.addEventListener('scroll', this._handleScroll, supportsPassive
-			? {
-				passive: true
-			}
-			: false)
-	}
-
-	_unbindScroll = () => {
-		window.removeEventListener('scroll', this._handleScroll)
-	}
-
-	_handleScroll = () => {
-		// Ugly cross-browser compatibility
-		const top = document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop
-		// Test < 1 since Safari's rebound effect scrolls past the top
-
-		console.log(this.base.firstChild);
-		if (top < 10) {
-			// const className = `${header.header}`
-			this.base.firstChild.className = 'header__3fP58'
-		} else {
-			// const className = `${header.header} ${header.scrolled}`
-			this.base.firstChild.className = 'header__3fP58 scroll__3hLWG'
-		}
-	}
-
   render() {
     return (
       <div className={style.scroll}>
 				<Header />
         <div className={style.videoWrapper}>
-          <iframe src="https://www.youtube.com/embed/HxOwaKulUlM?controls=0&showinfo=0&rel=0&autoplay=1&loop=1playlist=HxOwaKulUlM" frameborder="0" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/HxOwaKulUlM?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=HxOwaKulUlM" frameborder="0" allowfullscreen></iframe>
         </div>
 
         <div>test</div>
